26fca9c5ca9c30d8e3d71f24aa97b6534fcb7384
godard31 <105897735+godard31@users.noreply.github.com>
2026-03-11 17:20:40 -0400

    Update love_app.py

--- love_app.py
+++ love_app.py
@@ -5,9 +5,20 @@ things = [
     "You always smell good",  
     "You are stunningly beautiful",
     "Cheek kisses",
-    "Your eyes in the sunlight"
+    "Your eyes in the sunlight",
+    "You have the best dog in the world",
+    "You laugh at my poop jokes",
+    "You are so soft all the time",
+    "You make the best mac n cheese",
+    "You bring me weed and get high with me",
+    "You pluck my hairs",
+    "The way you breathe when you are asleep and it relaxes me",
+    "You dress so cute",
+    "Your lips and teeth when you laugh",
+    "You are a sandwich master"
     # ... add all 50+ here, one per line ...
 ]
+
 
 st.set_page_config(page_title="Things He Loves About me", page_icon="💕", layout="centered")
 st.markdown("## 💕 Things He Loves About Me 💕")
@@ -16,4 +27,5 @@ if st.button("😍 Tell me something he loves about me", use_container_width=Tru
     love = random.choice(things)
     st.markdown(f"**{love}** ❤️")
 else:
+
     st.markdown("*Tap the button for a surprise!*")
